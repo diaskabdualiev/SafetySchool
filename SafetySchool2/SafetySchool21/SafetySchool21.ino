17b127331240ca5b5caa4c0a5ef4899762f5218a
DiasKabdualiyev <dias.31407@gmail.com>
2024-03-07 22:03:06 +0500

    Все озуенно работает

---
+++ SafetySchool2/SafetySchool21/SafetySchool21.ino
@@ -0,0 +1,210 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "DFRobotDFPlayerMini.h"
+
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266))   // Using a soft serial port
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(/*rx =*/16, /*tx =*/17);
+#define FPSerial softSerial
+#else
+#define FPSerial Serial1
+#endif
+
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "SafetySchool"
+#define WIFI_PASSWORD "SafetySchool"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyCI9EJ6dAA-XN8wYl8Y2fiSZNJGX5AEqmM"
+
+// Insert RTDB URL
+#define DATABASE_URL "https://safetyschool-5bb16-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+// Define Firebase Data object
+FirebaseData stream;
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+int selectVal = 0;
+
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/16, /*tx =*/17);
+
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
+  //----Set volume----
+  myDFPlayer.volume(10);  //Set volume value (0~30).
+  myDFPlayer.volumeUp(); //Volume Up
+  myDFPlayer.volumeDown(); //Volume Down
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  if (!Firebase.RTDB.beginStream(&stream, "/esp2"))
+    Serial.printf("stream begin error, %s\n\n", stream.errorReason().c_str());
+  Firebase.RTDB.setStreamCallback(&stream, streamCallback, streamTimeoutCallback);
+//  myDFPlayer.play(1);
+}
+
+
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout) {
+    // Stream timeout occurred
+    Serial.println("Stream timeout, resume streaming...");
+  }
+}
+
+void streamCallback(FirebaseStream data)
+{
+  Serial.println("Получены данные из потока...");
+  Serial.println(data.streamPath());
+  Serial.println(data.dataPath());
+  Serial.println(data.dataType());
+
+  // Обработка для zharyq1 до zharyq15
+  if (data.dataPath().endsWith("/volume")) {
+    myDFPlayer.volume(data.intData());
+    Serial.print("volume: ");
+    Serial.println(data.intData());
+  }else if(data.dataPath().endsWith("/select")){
+    selectVal = data.intData();
+    Serial.print("Selected music: ");
+    Serial.println(selectVal);
+  }else if(data.dataPath().endsWith("/playStop")){
+    if(data.intData()){
+      myDFPlayer.pause();
+      Serial.print("Paused music");
+    }else{
+      myDFPlayer.play(selectVal);
+      Serial.print("Played music: ");
+      Serial.println(selectVal);
+    }
+  }
+
+  // Выводим значение
+  Serial.println(data.intData());
+}
+void loop() {
+
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 2000 || sendDataPrevMillis == 0)) {
+    sendDataPrevMillis = millis();
+    
+  }
+    if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  
+}
